08c8a02fc0bc1ed769c276923387e07dcdff9e1d
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-03 20:56:30 +0800

    simple product page with cards

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+
+function Product({product}) {
+    
+    return (
+        <Card className="my-3 p-3 rounded">
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image} />
+            </a>
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as="div">
+                    {product.rating} from {product.numReviews}
+                </Card.Text>
+
+                <Card.Text as="h3">
+                    ${product.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
