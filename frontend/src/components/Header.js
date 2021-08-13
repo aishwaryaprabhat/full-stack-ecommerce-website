96fd95d0342e505588f62c053ce752ed455f2970
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-13 23:41:43 +0800

    reactifying using link container for icons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Navbar, Container, Nav} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>Navbar scroll</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -14,8 +17,13 @@ function Header() {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                         >
-                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>   
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link ><i className="fas fa-user"></i>Login</Nav.Link>
+                        </LinkContainer>
+                        
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
