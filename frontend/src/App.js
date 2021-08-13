63bfc7a93d8ad1b676e43bae46dff5bf4f05f67a
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-13 23:31:36 +0800

    simple route example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,19 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+      <Router>
       <Header />
       <main className="py-3">
         <Container>
-        <HomeScreen />
+        <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </div>
+      </Router>
   );
 }
 
