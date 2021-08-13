ef1a884f3f32b4d222db08d6a89c6dca28d0b52d
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-13 23:36:01 +0800

    another route example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="py-3">
         <Container>
         <Route path="/" component={HomeScreen} exact />
+        <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
