114d8c765116f2fc3bcf7cbd577806b18e1fcaf4
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-14 22:39:18 +0800

    very simple routing for backend products data

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"), #home page
+    path('products/', views.getProducts, name="products"), #home page
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
 def getRoutes(request):
+
     return JsonResponse('Hello', safe=False)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
