a2ce24af75319e516e1fbd1bfd0d403ffb09d796
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-14 22:52:03 +0800

    simple example of djangorestframework

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"), #home page
     path('products/', views.getProducts, name="products"), #home page
+    path('products/<str:pk>', views.getProduct, name="product"), #home page
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .products import products
 
 # Create your views here.
+@api_view(['GET'])
 def getRoutes(request):
 
-    return JsonResponse('Hello', safe=False)
+    return Response('Hello')
 
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+
+    for prod in products:
+        if prod["_id"]==pk:
+            product = prod
+            break
+
+    return Response(product)
