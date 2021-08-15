203a9d5427675caf05283d19ec77de62d39531ee
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-15 10:43:26 +0800

    simple serialization example for returning model data

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -12,7 +15,9 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
