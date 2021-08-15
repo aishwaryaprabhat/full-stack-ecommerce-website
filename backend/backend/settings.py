0f92056db1be6bdb5916fc3754325f9cb92c5ccc
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-15 10:17:12 +0800

    handing user uploaded media using static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- backend/base/models.py
+++ backend/base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #creates relationship to user
     name = models.CharField(max_length = 200, null=True, blank=True)
-    # image = 
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length = 200, null=True, blank=True)
     category = models.CharField(max_length = 200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -18,3 +18,56 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True) #creates relationship to product
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #creates relationship to user
+    name = models.CharField(max_length = 200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #creates relationship to user
+    paymentMethod = models.CharField(max_length = 200, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.BooleanField(default=False)
+    deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True) #creates relationship to product
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True) #creates relationship to product
+    name = models.CharField(max_length = 200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length = 200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True) #if someone deletes an order there is no need to keep it 
+    address = models.CharField(max_length = 200, null=True, blank=True)
+    city = models.CharField(max_length = 200, null=True, blank=True)
+    postalCode = models.CharField(max_length = 200, null=True, blank=True)
+    country = models.CharField(max_length = 200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
