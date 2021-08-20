d16055000ca470c7036abe33353e20847427a7e4
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-20 22:18:17 +0800

    product details reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,10 @@
 import {PRODUCT_LIST_FAIL, 
         PRODUCT_LIST_REQUEST, 
-        PRODUCT_LIST_SUCCESS
+        PRODUCT_LIST_SUCCESS,
+
+        PRODUCT_DETAILS_REQUEST,
+        PRODUCT_DETAILS_SUCCESS,
+        PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
 export const productListReducer = (state={products:[]}, action) => {
@@ -12,6 +16,23 @@ export const productListReducer = (state={products:[]}, action) => {
             return {loading:false, products: action.payload}
         
         case PRODUCT_LIST_FAIL:
+            return {loading:false, error: action.payload}
+        
+        default:
+            return state
+
+    }
+}
+
+export const productDetailsReducer = (state={products:{reviews: []}}, action) => {
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading:true, ...state}
+        
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading:false, products: action.payload}
+        
+        case PRODUCT_DETAILS_FAIL:
             return {loading:false, error: action.payload}
         
         default:
