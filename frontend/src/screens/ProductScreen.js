b1489c254e99bd2c75165f5c5d15649b530b2032
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-21 00:26:26 +0800

    spinner loading example

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = ({match}) => {
@@ -19,6 +21,9 @@ const ProductScreen = ({match}) => {
     return (
         <div>
             <Link to="/" className="btn btn-light my-3">Go Back</Link>
+            {loading ? <Loader />
+                : error ? <Message variant="danger">{error}</Message>
+                : 
             <Row>
                 <Col md={6}>
                     <Image src={product.image} alr={product.name} fluid />
@@ -65,6 +70,7 @@ const ProductScreen = ({match}) => {
                     </Card>
                 </Col>
             </Row>
+}
         </div>
     )
 }
