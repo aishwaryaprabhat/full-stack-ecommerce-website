b26c3063de8a97c0be613fa0d3fffcb812f9b074
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-20 21:02:51 +0800

    simple redux, store, reducer, actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import {PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS
+} from '../constants/productConstants'
+
+export const listProducts = () => async (dispatch) =>{
+    try {
+        dispatch({type:PRODUCT_LIST_REQUEST})
+        const {data} = await axios.get('/api/products')
+        dispatch({
+            type:PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type:PRODUCT_LIST_FAIL,
+            payload:error.response && error.response.data.message ? error.reesponse.data.message : error.message
+        })
+    }
+}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,36 @@
 import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 
 import Product from '../components/Product'
-import axios from 'axios'
+import {listProducts} from '../actions/productActions'
 
 function HomeScreen() {
 
-    const [products, setProducts] = useState([])
-    useEffect(()=>{
-        async function fetchProducts(){
-            const {data} = await axios.get('/api/products')
-            setProducts(data)
-        }
+    const dispatch = useDispatch()
+    const productList = useSelector(state=>state.productList)
+    const {error, loading, products} = productList
 
-        fetchProducts()
+    useEffect(()=>{
+        dispatch(listProducts())
+        
     }, [])
 
     return (
         <div>
             <h1>Latest Products!</h1>
-            <Row>
+            {loading ? <h2>Loading...</h2>
+                : error ? <h2>{error}</h2>
+                : 
+                <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+                </Row>
+        }
+            
         </div>
     )
 }
