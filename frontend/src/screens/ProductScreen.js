b13683919754a64bec7e4a034ff4eec0b43280d8
Aishwarya <aishwaryaprabhat@gmail.com>
2021-08-22 17:17:24 +0800

    simple dropdown for selecting qty

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem, Form } from 'react-bootstrap'
 
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
@@ -9,6 +9,7 @@ import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = ({match}) => {
+    const [qty, setQty] = useState(1)
 
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
@@ -61,6 +62,29 @@ const ProductScreen = ({match}) => {
                                     <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
                                 </Row>
                             </ListGroupItem>
+                            {product.countInStock >0 && (
+                               <ListGroupItem>
+                                   <Row>
+                                       <Col>Qty</Col>
+                                       <Col xs='auto' className='my-1'>
+                                            <Form.Control
+                                            as="select"
+                                            value={qty}
+                                            onChange={(e) => setQty(e.target.value)}
+                                            >
+                                                {
+                                                    [...Array(product.countInStock).keys()].map((x) => (
+                                                        <option value={x+1} key={x+1}>
+                                                            {x+1}
+                                                        </option>
+                                                    ))
+                                                }
+
+                                            </Form.Control>
+                                       </Col>
+                                   </Row>
+                               </ListGroupItem> 
+                            )}
                             <ListGroupItem>
                                 <Row>
                                     <Button className='btn-block' type="button" disabled={product.countInStock==0}>ADD TO CART</Button>
